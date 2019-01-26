04c57d20739f7d6727fd770c1d7628289f903bba
Bruno Boucard <boucard.bruno@free.fr>
2019-01-26 11:54:05 +0100

    Rename SeatsNotAvailable to SuggestionNotAvailable.

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocator.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocator.cs
@@ -29,7 +29,7 @@
                 }
             }
 
-            return new SeatsNotAvailable(partyRequested);
+            return new SuggestionNotAvailable(partyRequested);
         }
     }
 }
--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SuggestionNotAvailable.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SuggestionNotAvailable.cs
@@ -5,9 +5,9 @@ namespace SeatsSuggestions.Tests
     /// <summary>
     ///     Occurs when a Suggestion that does not meet expectation is made.
     /// </summary>
-    public class SeatsNotAvailable : SuggestionMade
+    public class SuggestionNotAvailable : SuggestionMade
     {
-        public SeatsNotAvailable(int partyRequested) : base(partyRequested, new List<Seat>())
+        public SuggestionNotAvailable(int partyRequested) : base(partyRequested, new List<Seat>())
         {
         }
     }
