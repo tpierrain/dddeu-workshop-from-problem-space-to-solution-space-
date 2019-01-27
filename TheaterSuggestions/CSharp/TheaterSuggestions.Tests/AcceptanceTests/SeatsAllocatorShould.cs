18d6bae7e469d00f59f832b572b3b3b8dfa3deb8
Bruno Boucard <boucard.bruno@free.fr>
2019-01-27 16:58:30 +0100

    Add the two failing acceptance tests for lab3 (middle of the row)

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/AcceptanceTests/SeatsAllocatorShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/AcceptanceTests/SeatsAllocatorShould.cs
@@ -45,7 +45,7 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
         [Test]
         public void Offer_several_suggestions_ie_1_per_PricingCategory_and_other_one_without_category_affinity()
         {
-            const string eventId = "18";
+            const string showId = "18";
             const int partyRequested = 1;
 
             var auditoriumLayoutAdapter =
@@ -53,7 +53,7 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
 
             var seatAllocator = new SeatAllocator(auditoriumLayoutAdapter);
 
-            var suggestionsMade = seatAllocator.MakeSuggestions(eventId, partyRequested);
+            var suggestionsMade = seatAllocator.MakeSuggestions(showId, partyRequested);
 
             Check.That(suggestionsMade.SeatNames(PricingCategory.First)).ContainsExactly("A3", "A4", "A5");
             Check.That(suggestionsMade.SeatNames(PricingCategory.Second)).ContainsExactly("A1", "A2", "A9");
@@ -61,6 +61,41 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
 
             // BUG!!! => return A6, A7, A8 instead of the expected A1, A2, A3
             Check.That(suggestionsMade.SeatNames(PricingCategory.Mixed)).ContainsExactly("A1", "A2", "A3");
+        }
+
+        [Test]
+        public void Offer_adjacent_seats_nearer_the_middle_of_a_row()
+        {
+            const string showId = "9";
+            const int partyRequested = 1;
+
+            var auditoriumLayoutAdapter =
+                new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider());
+
+            var seatAllocator = new SeatAllocator(auditoriumLayoutAdapter);
+
+            var suggestionsMade = seatAllocator.MakeSuggestions(showId, partyRequested);
+
+            Check.That(suggestionsMade.SeatNames(PricingCategory.First)).ContainsExactly("A4", "A3", "B5");
+        }
+
+        [Test]
+        public void Offer_adjacent_seats_nearer_the_middle_of_a_row_when_it_is_possible()
+        {
+            const string showId = "3";
+            const int partyRequested = 4;
+
+            var auditoriumLayoutAdapter =
+                new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider());
+
+            var seatAllocator = new SeatAllocator(auditoriumLayoutAdapter);
+
+            var suggestionsMade = seatAllocator.MakeSuggestions(showId, partyRequested);
+
+            Check.That(suggestionsMade.SeatNames(PricingCategory.First)).IsEmpty();
+            Check.That(suggestionsMade.SeatNames(PricingCategory.Second)).ContainsExactly("C4", "C5", "C6", "C7");
+            Check.That(suggestionsMade.SeatNames(PricingCategory.Third)).ContainsExactly("E4", "E5", "E6", "E7");
+            Check.That(suggestionsMade.SeatNames(PricingCategory.Mixed)).ContainsExactly("A6", "A7", "A8", "A9");
         }
     }
 }
