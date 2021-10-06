97ae0b9846ca7e44b1fc6af355de9224ea1d14a9
Kenny Baas-Schwegler <kenny@baasie.com>
2021-10-06 12:54:48 +0200

    fixed compilation errors

--- TheaterSuggestions/Java/SeatsSuggestionsApi/src/main/java/com/baasie/SeatsSuggestionsApi/controllers/SeatSuggestionsController.java
+++ TheaterSuggestions/Java/SeatsSuggestionsApi/src/main/java/com/baasie/SeatsSuggestionsApi/controllers/SeatSuggestionsController.java
@@ -1,5 +1,7 @@
 package com.baasie.SeatsSuggestionsApi.controllers;
 
+import com.baasie.ExternalDependencies.auditoriumlayoutrepository.AuditoriumLayoutRepository;
+import com.baasie.ExternalDependencies.reservationsprovider.ReservationsProvider;
 import com.baasie.SeatsSuggestions.AuditoriumSeatingAdapter;
 import com.baasie.SeatsSuggestions.SeatAllocator;
 import com.baasie.SeatsSuggestions.SuggestionsMade;
@@ -7,21 +9,21 @@ import com.baasie.ExternalDependencies.IProvideAuditoriumLayouts;
 import com.baasie.ExternalDependencies.IProvideCurrentReservations;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("api/SeatsSuggestions")
 public class SeatSuggestionsController {
 
-    private IProvideAuditoriumLayouts auditoriumSeatingRepository;
-    private IProvideCurrentReservations seatReservationsProvider;
 
     public SeatSuggestionsController() {
     }
 
     // GET api/SeatsSuggestions?showId=5&party=3
     @GetMapping(produces = "application/json")
-    public SuggestionsMade get(@RequestParam String showId, @RequestParam int party) {
+    public SuggestionsMade get(@RequestParam String showId, @RequestParam int party) throws IOException {
 
-        SeatAllocator seatAllocator = new SeatAllocator(new AuditoriumSeatingAdapter(auditoriumSeatingRepository, seatReservationsProvider));
+        SeatAllocator seatAllocator = new SeatAllocator(new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider()));
         return seatAllocator.makeSuggestions(showId, party);
     }
 }
