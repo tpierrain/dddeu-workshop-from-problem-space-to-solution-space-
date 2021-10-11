d1b964e08d692fd95c5ecdef177fdbc50736eb92
Bruno Boucard <boucard.bruno@free.fr>
2021-10-11 14:59:22 +0200

    Add few tests based on middle of row to kick-off the labs on the deep model

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/RowShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/RowShould.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NFluent;
 using NUnit.Framework;
 
@@ -23,5 +24,77 @@ namespace SeatsSuggestions.Tests.UnitTests
             rowSecondInstance.AddSeat(A3);
             Check.That(rowSecondInstance).IsEqualTo(rowFirstInstance);
         }
+
+        [Test]
+        public void Offer_seas_from_the_middle_of_row_is_even_when_party_size_is_greater_than_one()
+        {
+            var partySize = 2;
+
+            var a1 = new Seat("A", 1, PricingCategory.Second, SeatAvailability.Available);
+            var a2 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
+            var a3 = new Seat("A", 3, PricingCategory.First, SeatAvailability.Available);
+            var a4 = new Seat("A", 4, PricingCategory.First, SeatAvailability.Reserved);
+            var a5 = new Seat("A", 5, PricingCategory.First, SeatAvailability.Available);
+            var a6 = new Seat("A", 6, PricingCategory.First, SeatAvailability.Available);
+            var a7 = new Seat("A", 7, PricingCategory.First, SeatAvailability.Available);
+            var a8 = new Seat("A", 8, PricingCategory.First, SeatAvailability.Reserved);
+            var a9 = new Seat("A", 9, PricingCategory.Second, SeatAvailability.Available);
+            var a10 = new Seat("A", 10, PricingCategory.Second, SeatAvailability.Available);
+
+            var row = new Row("A", new List<Seat> { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 });
+
+            var seatsWithDistance = OfferSeatsNearerTheMiddleOfTheRow(row, PricingCategory.Mixed).Take(partySize);
+
+            Check.That(seatsWithDistance.Select(s => s.Seat).ToList())
+                .ContainsExactly(a5, a6);
+        }
+
+
+        [Test]
+        public void Offer_seats_from_the_middle_of_row_is_odd_when_party_size_is_greater_than_one()
+        {
+            var partySize = 5;
+
+            var a1 = new Seat("A", 1, PricingCategory.Second, SeatAvailability.Available);
+            var a2 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
+            var a3 = new Seat("A", 3, PricingCategory.First, SeatAvailability.Available);
+            var a4 = new Seat("A", 4, PricingCategory.First, SeatAvailability.Reserved);
+            var a5 = new Seat("A", 5, PricingCategory.First, SeatAvailability.Available);
+            var a6 = new Seat("A", 6, PricingCategory.First, SeatAvailability.Available);
+            var a7 = new Seat("A", 7, PricingCategory.First, SeatAvailability.Available);
+            var a8 = new Seat("A", 8, PricingCategory.First, SeatAvailability.Reserved);
+            var a9 = new Seat("A", 9, PricingCategory.Second, SeatAvailability.Available);
+
+            var row = new Row("A", new List<Seat> { a1, a2, a3, a4, a5, a6, a7, a8, a9 });
+            var seatsWithDistance = OfferSeatsNearerTheMiddleOfTheRow(row, PricingCategory.Mixed).Take(partySize);
+
+            Check.That(seatsWithDistance.Select(s => s.Seat).OrderBy(s => s.Number).ToList())
+                .ContainsExactly(a2, a3, a5, a6, a7);
+        }
+
+        public IEnumerable<SeatWithTheDistanceFromTheMiddleOfTheRow> OfferSeatsNearerTheMiddleOfTheRow(Row row, PricingCategory pricingCategory)
+        {
+            // TODO: Implement your logic here
+
+            return new List<SeatWithTheDistanceFromTheMiddleOfTheRow>();
+        }
+    }
+
+    public class SeatWithTheDistanceFromTheMiddleOfTheRow
+    {
+        public SeatWithTheDistanceFromTheMiddleOfTheRow(Seat seat, int distanceFromTheMiddleOfTheRow)
+        {
+            Seat = seat;
+            DistanceFromTheMiddleOfTheRow = distanceFromTheMiddleOfTheRow;
+        }
+
+        public Seat Seat { get; }
+        public int DistanceFromTheMiddleOfTheRow { get; }
+
+        public override string ToString()
+        {
+            return $"{Seat.RowName}{Seat.Number} {DistanceFromTheMiddleOfTheRow}";
+        }
+
     }
 }
