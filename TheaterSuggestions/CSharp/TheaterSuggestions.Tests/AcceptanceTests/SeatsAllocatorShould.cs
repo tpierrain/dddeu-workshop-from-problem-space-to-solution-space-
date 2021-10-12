0c6bb44c523fc05c914b3952020c321b7905a259
Bruno Boucard <boucard.bruno@free.fr>
2021-10-12 14:07:39 +0200

    refactoring

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/AcceptanceTests/SeatsAllocatorShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/AcceptanceTests/SeatsAllocatorShould.cs
@@ -14,8 +14,8 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
         [Test]
         public async Task Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved()
         {
-            var showId = new ShowId("5");
-            var partyRequested = new PartyRequested(1);
+            ShowId showId = new ShowId("5");
+            PartyRequested partyRequested = new PartyRequested(1);
 
             var auditoriumLayoutAdapter =
                 new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider());
@@ -32,8 +32,8 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
         [Test]
         public async Task Suggest_one_seat_when_Auditorium_contains_one_available_seat_only()
         {
-            var showId = new ShowId("1");
-            var partyRequested = new PartyRequested(1);
+            ShowId showId = new ShowId("1");
+            PartyRequested partyRequested = new PartyRequested(1);
 
             var auditoriumLayoutAdapter =
                 new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider());
@@ -48,8 +48,8 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
         [Test]
         public async Task Offer_several_suggestions_ie_1_per_PricingCategory_and_other_one_without_category_affinity()
         {
-            var showId = new ShowId("18");
-            var partyRequested = new PartyRequested(1);
+            ShowId showId = new ShowId("18");
+            PartyRequested partyRequested = new PartyRequested(1);
 
             var auditoriumLayoutAdapter =
                 new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider());
@@ -60,16 +60,17 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
 
             Check.That(suggestionsMade.SeatNames(PricingCategory.First)).ContainsExactly("A5", "A6", "A4");
             Check.That(suggestionsMade.SeatNames(PricingCategory.Second)).ContainsExactly("A2", "A1", "A9");
+
             Check.That(suggestionsMade.SeatNames(PricingCategory.Third)).ContainsExactly("E5", "E6", "E4");
 
             Check.That(suggestionsMade.SeatNames(PricingCategory.Mixed)).ContainsExactly("A5", "A6", "A4");
         }
 
         [Test]
-        public async Task Offer_seats_nearer_the_middle_of_a_row()
+        public async Task Offer_adjacent_seats_nearer_the_middle_of_a_row()
         {
-            var showId = new ShowId("9");
-            var partyRequested = new PartyRequested(1);
+            ShowId showId = new ShowId("9");
+            PartyRequested partyRequested = new PartyRequested(1);
 
             var auditoriumLayoutAdapter =
                 new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider());
@@ -84,7 +85,7 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
         [Test]
         public async Task Offer_adjacent_seats_nearer_the_middle_of_a_row_when_it_is_possible()
         {
-            var showId = new ShowId("3");
+            ShowId showId = new ShowId("3");
             PartyRequested partyRequested = new PartyRequested(4);
 
             var auditoriumLayoutAdapter =
@@ -95,12 +96,9 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
             var suggestionsMade = await seatAllocator.MakeSuggestions(showId, partyRequested);
 
             Check.That(suggestionsMade.SeatNames(PricingCategory.First)).IsEmpty();
-            Check.That(suggestionsMade.SeatNames(PricingCategory.Second))
-                .ContainsExactly("C4-C5-C6-C7", "D4-D5-D6-D7");
-            Check.That(suggestionsMade.SeatNames(PricingCategory.Third))
-                .ContainsExactly("E4-E5-E6-E7", "F4-F5-F6-F7");
-            Check.That(suggestionsMade.SeatNames(PricingCategory.Mixed))
-                .ContainsExactly("A6-A7-A8-A9", "B1-B2-B3-B4", "C4-C5-C6-C7");
+            Check.That(suggestionsMade.SeatNames(PricingCategory.Second)).ContainsExactly("C4-C5-C6-C7", "D4-D5-D6-D7");
+            Check.That(suggestionsMade.SeatNames(PricingCategory.Third)).ContainsExactly("E4-E5-E6-E7", "F4-F5-F6-F7");
+            Check.That(suggestionsMade.SeatNames(PricingCategory.Mixed)).ContainsExactly("A6-A7-A8-A9", "B1-B2-B3-B4", "C4-C5-C6-C7");
         }
     }
 }
