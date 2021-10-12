c4d99d1c6d1ab9fc595fd541177239324f728964
Bruno Boucard <boucard.bruno@free.fr>
2021-10-12 07:36:11 +0200

    rename unit-tests form for seats nearer the middle of the row and remove acceptance for adjacent seats

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/AcceptanceTests/SeatsAllocatorShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/AcceptanceTests/SeatsAllocatorShould.cs
@@ -56,14 +56,14 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
             var suggestionsMade = seatAllocator.MakeSuggestions(showId, partyRequested);
 
             Check.That(suggestionsMade.SeatNames(PricingCategory.First)).ContainsExactly("A5", "A6", "A4");
-            Check.That(suggestionsMade.SeatNames(PricingCategory.Second)).ContainsExactly( "A2", "A9", "A1");
+            Check.That(suggestionsMade.SeatNames(PricingCategory.Second)).ContainsExactly("A2", "A9", "A1");
             Check.That(suggestionsMade.SeatNames(PricingCategory.Third)).ContainsExactly("E5", "E6", "E4");
 
             Check.That(suggestionsMade.SeatNames(PricingCategory.Mixed)).ContainsExactly("A5", "A6", "A4");
         }
 
         [Test]
-        public void Offer_adjacent_seats_nearer_the_middle_of_a_row()
+        public void Offer_seats_nearer_the_middle_of_a_row()
         {
             // FIX ME
             const string showId = "9";
@@ -77,29 +77,6 @@ namespace SeatsSuggestions.Tests.AcceptanceTests
             var suggestionsMade = seatAllocator.MakeSuggestions(showId, partyRequested);
 
             Check.That(suggestionsMade.SeatNames(PricingCategory.First)).ContainsExactly("A4", "A3", "B5");
-        }
-
-        [Test, Ignore("not yet")]
-        public void Offer_adjacent_seats_nearer_the_middle_of_a_row_when_it_is_possible()
-        {
-            // FIX ME
-            const string showId = "3";
-            const int partyRequested = 4;
-
-            var auditoriumLayoutAdapter =
-                new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider());
-
-            var seatAllocator = new SeatAllocator(auditoriumLayoutAdapter);
-
-            var suggestionsMade = seatAllocator.MakeSuggestions(showId, partyRequested);
-
-            Check.That(suggestionsMade.SeatNames(PricingCategory.First)).IsEmpty();
-            Check.That(suggestionsMade.SeatNames(PricingCategory.Second))
-                .ContainsExactly("C4-C5-C6-C7", "D4-D5-D6-D7");
-            Check.That(suggestionsMade.SeatNames(PricingCategory.Third))
-                .ContainsExactly("E4-E5-E6-E7", "F4-F5-F6-F7");
-            Check.That(suggestionsMade.SeatNames(PricingCategory.Mixed))
-                .ContainsExactly("A6-A7-A8-A9", "C4-C5-C6-C7", "D4-D5-D6-D7");
         }
     }
 }
