1fb60f88651e6afe0a24f3bfba15adfbdfe998b1
Bruno Boucard <boucard.bruno@free.fr>
2021-10-12 14:34:49 +0200

    add theater exemples

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatsAllocatorShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatsAllocatorShould.cs
@@ -11,6 +11,11 @@ namespace SeatsSuggestions.Tests
         [Test]
         public void Suggest_one_seat_when_Auditorium_contains_one_available_seat_only()
         {
+            // Ford Auditorium-1
+            //
+            //       1   2   3   4   5   6   7   8   9  10
+            //  A : (2) (2)  1  (1) (1) (1) (1) (1) (2) (2)
+            //  B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
             const string showId = "1";
             const int partyRequested = 1;
 
@@ -28,6 +33,15 @@ namespace SeatsSuggestions.Tests
         [Test]
         public void Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved()
         {
+            // Madison Auditorium-5
+            //
+            //     1   2   3   4   5   6   7   8   9  10
+            //  A: 2   2   1   1   1   1   1   1   2   2
+            //  B: 2   2   1   1   1   1   1   1   2   2
+            //  C: 2   2   2   2   2   2   2   2   2   2
+            //  D: 2   2   2   2   2   2   2   2   2   2
+            //  E: 3   3   3   3   3  (3)  3   3   3   3
+            //  F: 3   3   3   3   3   3   3   3   3   3
             const string showId = "5";
             const int partyRequested = 1;
 
