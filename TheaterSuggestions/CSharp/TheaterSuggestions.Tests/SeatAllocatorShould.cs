55a6f3e3410e9e0459b627d94e1b9c2401118962
Bruno Boucard <boucard.bruno@free.fr>
2021-10-12 14:51:44 +0200

    refactoring

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocatorShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocatorShould.cs
@@ -34,14 +34,9 @@ namespace SeatsSuggestions.Tests
         public void Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved()
         {
             // Madison Auditorium-5
-            //
-            //     1   2   3   4   5   6   7   8   9  10
-            //  A: 2   2   1   1   1   1   1   1   2   2
-            //  B: 2   2   1   1   1   1   1   1   2   2
-            //  C: 2   2   2   2   2   2   2   2   2   2
-            //  D: 2   2   2   2   2   2   2   2   2   2
-            //  E: 3   3   3   3   3  (3)  3   3   3   3
-            //  F: 3   3   3   3   3   3   3   3   3   3
+            //      1   2   3   4   5   6   7   8   9  10
+            // A : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
+            // B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
             const string showId = "5";
             const int partyRequested = 1;
 
