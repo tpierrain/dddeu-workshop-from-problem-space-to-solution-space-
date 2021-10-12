e11edbb77a3593a538f6251e83ff05038f700c3d
Bruno Boucard <boucard.bruno@free.fr>
2021-10-12 19:13:35 +0200

    typos

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/DeepModel/OfferingSeatsNearerMiddleOfTheRowShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/DeepModel/OfferingSeatsNearerMiddleOfTheRowShould.cs
@@ -30,7 +30,7 @@ namespace SeatsSuggestions.Tests.UnitTests.DeepModel
         }
 
         [Test]
-        public void Offer_seats_from_the_middle_of_the_row_wih_row_size_even_when_party_size_is_greater_than_one()
+        public void Offer_seats_from_the_middle_of_the_row_when_the_row_size_is_even_and_party_size_is_greater_than_one()
         {
             var partySize = 2;
 
@@ -55,7 +55,7 @@ namespace SeatsSuggestions.Tests.UnitTests.DeepModel
         }
 
         [Test]
-        public void Offer_seats_from_the_middle_of_the_row_with_row_size_odd_when_party_size_is_greater_than_one()
+        public void Offer_seats_from_the_middle_of_the_row_when_the_row_size_is_and_and_party_size_is_greater_than_one()
         {
             var partySize = 5;
 
