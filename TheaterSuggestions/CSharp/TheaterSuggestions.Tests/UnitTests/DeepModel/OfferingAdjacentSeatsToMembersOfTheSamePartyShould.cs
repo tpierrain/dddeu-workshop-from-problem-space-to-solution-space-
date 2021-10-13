ed8771574731985238aaa003ea95c821f033241c
Bruno Boucard <boucard.bruno@free.fr>
2021-10-13 09:08:18 +0200

    typos

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/DeepModel/OfferingAdjacentSeatsToMembersOfTheSamePartyShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/DeepModel/OfferingAdjacentSeatsToMembersOfTheSamePartyShould.cs
@@ -21,33 +21,7 @@ namespace SeatsSuggestions.Tests.UnitTests.DeepModel
         }
 
         [Test]
-        public void Offer_groups_of_adjacent_seats_when_row_contains_some_reserved_seats()
-        {
-            var partyRequested = new PartyRequested(3);
-            var a1 = new Seat("A", 1, PricingCategory.Second, SeatAvailability.Available);
-            var a2 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
-            var a3 = new Seat("A", 3, PricingCategory.First, SeatAvailability.Available);
-            var a4 = new Seat("A", 4, PricingCategory.First, SeatAvailability.Reserved);
-            var a5 = new Seat("A", 5, PricingCategory.First, SeatAvailability.Available);
-            var a6 = new Seat("A", 6, PricingCategory.First, SeatAvailability.Available);
-            var a7 = new Seat("A", 7, PricingCategory.First, SeatAvailability.Available);
-            var a8 = new Seat("A", 8, PricingCategory.First, SeatAvailability.Reserved);
-            var a9 = new Seat("A", 9, PricingCategory.Second, SeatAvailability.Available);
-            var a10 = new Seat("A", 10, PricingCategory.Second, SeatAvailability.Available);
-
-            var row = new Row("A", new List<Seat> { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 });
-            var seatsWithDistance =
-                new OfferingSeatsNearerMiddleOfTheRow(row).OfferSeatsNearerTheMiddleOfTheRow(
-                    new SuggestionRequest(partyRequested, PricingCategory.Mixed)).Take(partyRequested.PartySize);
-
-            Check.That(new OfferingAdjacentSeatsToMembersOfTheSameParty(new SuggestionRequest(partyRequested,
-                    PricingCategory.Mixed))
-                .OfferAdjacentSeats(seatsWithDistance)).ContainsExactly(a5, a6, a7);
-        }
-
-
-        [Test]
-        public void Offer_adjacent_seats_nearer_the_middle_of_row()
+        public void Offer_adjacent_seats_nearer_the_middle_of_the_row_when_the_middle_is_not_reserved()
         {
             var a1 = new Seat("A", 1, PricingCategory.Second, SeatAvailability.Available);
             var a2 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
@@ -67,7 +41,7 @@ namespace SeatsSuggestions.Tests.UnitTests.DeepModel
         }
 
         [Test]
-        public void Offer_adjacent_seats_nearer_the_middle_of_row_when_the_middle_is_reserve()
+        public void Offer_adjacent_seats_nearer_the_middle_of_the_row_when_the_middle_is_already_reserved()
         {
             var a1 = new Seat("A", 1, PricingCategory.Second, SeatAvailability.Available);
             var a2 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
