1fb13fc68204c7953803451ad94eb18799d4730d
Bruno Boucard <boucard.bruno@free.fr>
2021-10-14 05:41:43 +0200

    auditorium examples to illustrate each tests.

--- TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatAllocatorShould.java
+++ TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatAllocatorShould.java
@@ -9,11 +9,15 @@ import java.io.IOException;
 
 import static com.google.common.truth.Truth.assertThat;
 
-public class SeatsAllocatorShould {
-
+public class SeatAllocatorShould {
 
     @Test
     public void Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved() throws IOException {
+        // Madison Auditorium-5
+        //
+        //      1   2   3   4   5   6   7   8   9  10
+        // A : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
+        // B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
         final String showId = "5";
         final int partyRequested = 1;
 
@@ -31,6 +35,11 @@ public class SeatsAllocatorShould {
 
     @Test
     public void Suggest_one_seat_when_Auditorium_contains_one_available_seat_only() throws IOException {
+        // Ford Auditorium-1
+        //
+        //       1   2   3   4   5   6   7   8   9  10
+        //  A : (2) (2)  1  (1) (1) (1) (1) (1) (2) (2)
+        //  B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
         final String showId = "1";
         final int partyRequested = 1;
 
@@ -46,6 +55,15 @@ public class SeatsAllocatorShould {
 
     @Test
     public void Offer_several_suggestions_ie_1_per_PricingCategory_and_other_one_without_category_affinity() throws IOException {
+        // New Amsterdam-18
+        //
+        //     1   2   3   4   5   6   7   8   9  10
+        //  A: 2   2   1   1   1   1   1   1   2   2
+        //  B: 2   2   1   1   1   1   1   1   2   2
+        //  C: 2   2   2   2   2   2   2   2   2   2
+        //  D: 2   2   2   2   2   2   2   2   2   2
+        //  E: 3   3   3   3   3   3   3   3   3   3
+        //  F: 3   3   3   3   3   3   3   3   3   3
         final String showId = "18";
         final int partyRequested = 1;
 
