213f1ef9b56da07841d1991c8dddc74f3b2ad723
Bruno Boucard <boucard.bruno@free.fr>
2021-10-14 14:09:30 +0200

    implementation of calculates seats closest to the middle of the row identical to the C # version.

--- TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatsAllocatorShould.java
+++ TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatsAllocatorShould.java
@@ -14,6 +14,11 @@ public class SeatsAllocatorShould {
 
     @Test
     public void Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved() throws IOException {
+        // Madison Auditorium-5
+        //
+        //      1   2   3   4   5   6   7   8   9  10
+        // A : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
+        // B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
         final String showId = "5";
         final int partyRequested = 1;
 
@@ -29,6 +34,11 @@ public class SeatsAllocatorShould {
 
     @Test
     public void Suggest_one_seat_when_Auditorium_contains_one_available_seat_only() throws IOException {
+        // Ford Auditorium-1
+        //
+        //       1   2   3   4   5   6   7   8   9  10
+        //  A : (2) (2)  1  (1) (1) (1) (1) (1) (2) (2)
+        //  B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
         final String showId = "1";
         final int partyRequested = 1;
 
@@ -44,6 +54,15 @@ public class SeatsAllocatorShould {
 
     @Test
     public void Offer_several_suggestions_ie_1_per_PricingCategory_and_other_one_without_category_affinity() throws IOException {
+        // New Amsterdam-18
+        //
+        //     1   2   3   4   5   6   7   8   9  10
+        //  A: 2   2   1   1   1   1   1   1   2   2
+        //  B: 2   2   1   1   1   1   1   1   2   2
+        //  C: 2   2   2   2   2   2   2   2   2   2
+        //  D: 2   2   2   2   2   2   2   2   2   2
+        //  E: 3   3   3   3   3   3   3   3   3   3
+        //  F: 3   3   3   3   3   3   3   3   3   3
         final String showId = "18";
         final int partyRequested = 1;
 
@@ -54,16 +73,22 @@ public class SeatsAllocatorShould {
 
         SuggestionsMade suggestionsMade = seatAllocator.makeSuggestions(showId, partyRequested);
 
-        assertThat(suggestionsMade.seatNames(PricingCategory.First)).containsExactly("A3", "A4", "A5");
-        assertThat(suggestionsMade.seatNames(PricingCategory.Second)).containsExactly("A1", "A2", "A9");
-        assertThat(suggestionsMade.seatNames(PricingCategory.Third)).containsExactly("E1", "E2", "E3");
+        assertThat(suggestionsMade.seatNames(PricingCategory.First)).containsExactly("A5", "A6", "A4");
+        assertThat(suggestionsMade.seatNames(PricingCategory.Second)).containsExactly("A2", "A9", "A1");
+        assertThat(suggestionsMade.seatNames(PricingCategory.Third)).containsExactly("E5", "E6", "E4");
 
-        assertThat(suggestionsMade.seatNames(PricingCategory.Mixed)).containsExactly("A1", "A2", "A3");
+        assertThat(suggestionsMade.seatNames(PricingCategory.Mixed)).containsExactly("A5", "A6", "A4");
     }
 
     @Test
     public void Offer_adjacent_seats_nearer_the_middle_of_a_row() throws IOException {
         // FIX ME
+
+        // Mogador Auditorium-9
+        //
+        //    1   2   3   4   5   6   7   8   9  10
+        // A: 2   2   1   1  (1) (1) (1) (1)  2   2
+        // B: 2   2   1   1   1   1   1   1   2   2
         final String showId = "9";
         final int partyRequested = 1;
 
@@ -76,27 +101,4 @@ public class SeatsAllocatorShould {
 
         assertThat(suggestionsMade.seatNames(PricingCategory.First)).containsExactly("A4", "A3", "B5");
     }
-
-    @Test
-    public void Offer_adjacent_seats_nearer_the_middle_of_a_row_when_it_is_possible() throws IOException {
-        // FIX ME
-        final String showId = "3";
-        final int partyRequested = 4;
-
-        AuditoriumSeatingAdapter auditoriumLayoutAdapter =
-                new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider());
-
-        SeatAllocator seatAllocator = new SeatAllocator(auditoriumLayoutAdapter);
-
-        SuggestionsMade suggestionsMade = seatAllocator.makeSuggestions(showId, partyRequested);
-
-        assertThat(suggestionsMade.seatNames(PricingCategory.First)).isEmpty();
-        assertThat(suggestionsMade.seatNames(PricingCategory.Second))
-                .containsExactly("C5-C6-C7-C8", "C1-C2-C3-C4", "D5-D6-D7-D8");
-        assertThat(suggestionsMade.seatNames(PricingCategory.Third))
-                .containsExactly("E5-E6-E7-E8", "E1-E2-E3-E4", "F5-F6-F7-F8");
-        assertThat(suggestionsMade.seatNames(PricingCategory.Mixed))
-                .containsExactly("A6-A7-A8-A9", "B1-B2-B3-B4", "C5-C6-C7-C8");
-    }
-
 }
