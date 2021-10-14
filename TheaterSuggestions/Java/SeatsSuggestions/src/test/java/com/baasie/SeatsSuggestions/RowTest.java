62b3543f76ec01a99fa2854a421c5c04ad0030f3
Bruno Boucard <boucard.bruno@free.fr>
2021-10-14 06:52:17 +0200

    Added two tests on the RowShould class to help implement the rule of suggesting seats in the middle of the row.

--- TheaterSuggestions/Java/SeatsSuggestions/src/test/java/com/baasie/SeatsSuggestions/RowTest.java
+++ TheaterSuggestions/Java/SeatsSuggestions/src/test/java/com/baasie/SeatsSuggestions/RowTest.java
@@ -2,7 +2,11 @@ package com.baasie.SeatsSuggestions;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -24,4 +28,57 @@ public class RowTest {
         assertThat(rowSecondInstance).isEqualTo(rowFirstInstance);
     }
 
+    @Test
+    public void Offer_seats_from_the_middle_of_the_row_when_the_row_size_is_even_and_party_size_is_greater_than_one() {
+        int partySize = 2;
+
+        Seat a1 = new Seat("A", 1, PricingCategory.Second, SeatAvailability.Available);
+        Seat a2 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
+        Seat a3 = new Seat("A", 3, PricingCategory.First, SeatAvailability.Available);
+        Seat a4 = new Seat("A", 4, PricingCategory.First, SeatAvailability.Reserved);
+        Seat a5 = new Seat("A", 5, PricingCategory.First, SeatAvailability.Available);
+        Seat a6 = new Seat("A", 6, PricingCategory.First, SeatAvailability.Available);
+        Seat a7 = new Seat("A", 7, PricingCategory.First, SeatAvailability.Available);
+        Seat a8 = new Seat("A", 8, PricingCategory.First, SeatAvailability.Reserved);
+        Seat a9 = new Seat("A", 9, PricingCategory.Second, SeatAvailability.Available);
+        Seat a10 = new Seat("A", 10, PricingCategory.Second, SeatAvailability.Available);
+
+        Row row = new Row("A", new ArrayList<>(Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)));
+
+        List<SeatWithTheDistanceFromTheMiddleOfTheRow> seatsWithDistance = OfferSeatsNearerTheMiddleOfTheRow(row, PricingCategory.Mixed).stream().limit(partySize).collect(Collectors.toList());
+
+        List<Seat> seats = seatsWithDistance.stream().map(SeatWithTheDistanceFromTheMiddleOfTheRow::getSeat).collect(Collectors.toList());
+
+        assertThat(seats).containsExactly(a5, a6);
+    }
+
+
+    @Test
+    public void Offer_seats_from_the_middle_of_the_row_when_with_the_row_size_is_odd_and_party_size_is_greater_than_one() {
+        int partySize = 5;
+
+        Seat a1 = new Seat("A", 1, PricingCategory.Second, SeatAvailability.Available);
+        Seat a2 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
+        Seat a3 = new Seat("A", 3, PricingCategory.First, SeatAvailability.Available);
+        Seat a4 = new Seat("A", 4, PricingCategory.First, SeatAvailability.Reserved);
+        Seat a5 = new Seat("A", 5, PricingCategory.First, SeatAvailability.Available);
+        Seat a6 = new Seat("A", 6, PricingCategory.First, SeatAvailability.Available);
+        Seat a7 = new Seat("A", 7, PricingCategory.First, SeatAvailability.Available);
+        Seat a8 = new Seat("A", 8, PricingCategory.First, SeatAvailability.Reserved);
+        Seat a9 = new Seat("A", 9, PricingCategory.Second, SeatAvailability.Available);
+
+        Row row = new Row("A", new ArrayList<>(Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8, a9)));
+
+        List<SeatWithTheDistanceFromTheMiddleOfTheRow> seatsWithDistance =
+                OfferSeatsNearerTheMiddleOfTheRow(row, PricingCategory.Mixed).stream().limit(partySize).collect(Collectors.toList());
+
+        List<Seat> seats = seatsWithDistance.stream().map(SeatWithTheDistanceFromTheMiddleOfTheRow::getSeat).sorted(Comparator.comparingInt(Seat::number)).collect(Collectors.toList());
+
+        assertThat(seats).containsExactly(a2, a3, a5, a6, a7);
+    }
+
+    public List<SeatWithTheDistanceFromTheMiddleOfTheRow> OfferSeatsNearerTheMiddleOfTheRow(Row row, PricingCategory pricingCategory) {
+        // TODO: Implement your logic here
+        return new ArrayList<>();
+    }
 }
---
+++ TheaterSuggestions/Java/SeatsSuggestions/src/test/java/com/baasie/SeatsSuggestions/SeatWithTheDistanceFromTheMiddleOfTheRow.java
@@ -0,0 +1,24 @@
+package com.baasie.SeatsSuggestions;
+
+public class SeatWithTheDistanceFromTheMiddleOfTheRow {
+    Seat seat;
+    int distanceFromTheMiddleOfTheRow;
+
+    public SeatWithTheDistanceFromTheMiddleOfTheRow(Seat seat, int distanceFromTheMiddleOfTheRow) {
+        this.seat = seat;
+        this.distanceFromTheMiddleOfTheRow = distanceFromTheMiddleOfTheRow;
+    }
+
+    public Seat getSeat() {
+        return this.seat;
+    }
+
+    public int getDistanceFromTheMiddleOfTheRow() {
+        return this.distanceFromTheMiddleOfTheRow;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(this.seat.rowName() + this.seat.number() + " " + this.distanceFromTheMiddleOfTheRow);
+    }
+}
