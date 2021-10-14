2689409a8a670b36d491a52e5e77c32078dce0a3
Bruno Boucard <boucard.bruno@free.fr>
2021-10-14 05:31:23 +0200

    auditorium examples to illustrate the tests to be aligned with the previous lab.

--- TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatAllocatorShould.java
+++ TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatAllocatorShould.java
@@ -8,10 +8,15 @@ import java.io.IOException;
 
 import static com.google.common.truth.Truth.assertThat;
 
-public class SeatsAllocatorShould {
+public class SeatAllocatorShould {
 
     @Test
     public void Suggest_one_seat_when_Auditorium_contains_one_available_seat_only() throws IOException {
+        // Ford Auditorium-1
+        //
+        //       1   2   3   4   5   6   7   8   9  10
+        //  A : (2) (2)  1  (1) (1) (1) (1) (1) (2) (2)
+        //  B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
         final String showId = "1";
         final int partyRequested = 1;
 
@@ -28,6 +33,10 @@ public class SeatsAllocatorShould {
 
     @Test
     public void Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved() throws IOException {
+        // Madison Auditorium-5
+        //      1   2   3   4   5   6   7   8   9  10
+        // A : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
+        // B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
         final String showId = "5";
         final int partyRequested = 1;
 
