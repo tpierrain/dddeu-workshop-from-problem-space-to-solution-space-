c03d21541b3f2819e20ee6437ab96a16179b669f
Bruno Boucard <boucard.bruno@free.fr>
2021-10-16 06:58:44 +0200

    refactoring

--- TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatsAllocatorShould.java
+++ TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatsAllocatorShould.java
@@ -13,7 +13,12 @@ import static com.google.common.truth.Truth.assertThat;
 public class SeatsAllocatorShould {
 
     @Test
-    public void Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved() throws IOException {
+    public void return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved() throws IOException {
+        // Madison Auditorium-5
+        //
+        //      1   2   3   4   5   6   7   8   9  10
+        // A : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
+        // B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
         final String showId = "5";
         final int partyRequested = 1;
 
@@ -29,6 +34,11 @@ public class SeatsAllocatorShould {
 
     @Test
     public void Suggest_one_seat_when_Auditorium_contains_one_available_seat_only() throws IOException {
+        // Ford Auditorium-1
+        //
+        //       1   2   3   4   5   6   7   8   9  10
+        //  A : (2) (2)  1  (1) (1) (1) (1) (1) (2) (2)
+        //  B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
         final String showId = "1";
         final int partyRequested = 1;
 
@@ -43,7 +53,16 @@ public class SeatsAllocatorShould {
     }
 
     @Test
-    public void Offer_several_suggestions_ie_1_per_PricingCategory_and_other_one_without_category_affinity() throws IOException {
+    public void offer_several_suggestions_ie_1_per_PricingCategory_and_other_one_without_category_affinity() throws IOException {
+        // New Amsterdam-18
+        //
+        //     1   2   3   4   5   6   7   8   9  10
+        //  A: 2   2   1   1   1   1   1   1   2   2
+        //  B: 2   2   1   1   1   1   1   1   2   2
+        //  C: 2   2   2   2   2   2   2   2   2   2
+        //  D: 2   2   2   2   2   2   2   2   2   2
+        //  E: 3   3   3   3   3   3   3   3   3   3
+        //  F: 3   3   3   3   3   3   3   3   3   3
         final String showId = "18";
         final int partyRequested = 1;
 
@@ -62,7 +81,12 @@ public class SeatsAllocatorShould {
     }
 
     @Test
-    public void Offer_adjacent_seats_nearer_the_middle_of_a_row() throws IOException {
+    public void offer_adjacent_seats_nearer_the_middle_of_a_row() throws IOException {
+        // Mogador Auditorium-9
+        //
+        //    1   2   3   4   5   6   7   8   9  10
+        // A: 2   2   1   1  (1) (1) (1) (1)  2   2
+        // B: 2   2   1   1   1   1   1   1   2   2
         final String showId = "9";
         final int partyRequested = 1;
 
@@ -76,7 +100,17 @@ public class SeatsAllocatorShould {
         assertThat(suggestionsMade.seatNames(PricingCategory.First)).containsExactly("A4", "A3", "B5");
     }
     @Test
-    public void Offer_4_adjacent_seats_nearer_the_middle_of_a_row_when_it_is_possible() throws IOException, URISyntaxException {
+    public void offer_4_adjacent_seats_nearer_the_middle_of_a_row_when_it_is_possible() throws IOException, URISyntaxException {
+        //
+        // Dock Street Auditorium-3
+        //
+        //      1   2   3   4   5   6   7   8   9  10
+        // A : (2) (2) (1) (1) (1)  1   1   1   2   2
+        // B :  2   2   1   1  (1) (1) (1) (1)  2   2
+        // C :  2   2   2   2   2   2   2   2   2   2
+        // D :  2   2   2   2   2   2   2   2   2   2
+        // E :  3   3   3   3   3   3   3   3   3   3
+        // F :  3   3   3   3   3   3   3   3   3   3
         final String showId = "3";
         final int partyRequested = 4;
 
@@ -98,7 +132,17 @@ public class SeatsAllocatorShould {
 
 
     @Test
-    public void Offer_3_adjacent_seats_nearer_the_middle_of_a_row_when_it_is_possible() throws IOException, URISyntaxException {
+    public void offer_3_adjacent_seats_nearer_the_middle_of_a_row_when_it_is_possible() throws IOException, URISyntaxException {
+        //
+        // Dock Street Auditorium-3
+        //
+        //      1   2   3   4   5   6   7   8   9  10
+        // A : (2) (2) (1) (1) (1)  1   1   1   2   2
+        // B :  2   2   1   1  (1) (1) (1) (1)  2   2
+        // C :  2   2   2   2   2   2   2   2   2   2
+        // D :  2   2   2   2   2   2   2   2   2   2
+        // E :  3   3   3   3   3   3   3   3   3   3
+        // F :  3   3   3   3   3   3   3   3   3   3
         final String showId = "3";
         final int partyRequested = 3;
 
