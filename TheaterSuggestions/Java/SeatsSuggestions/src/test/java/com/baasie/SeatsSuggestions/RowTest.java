4688b700b2f4e187a7226e3d345f3292b384a90b
Bruno Boucard <boucard.bruno@free.fr>
2021-10-17 10:09:46 +0200

    refactoring deep model

--- TheaterSuggestions/Java/SeatsSuggestions/src/test/java/com/baasie/SeatsSuggestions/RowTest.java
+++ TheaterSuggestions/Java/SeatsSuggestions/src/test/java/com/baasie/SeatsSuggestions/RowTest.java
@@ -25,10 +25,11 @@ public class RowTest {
         assertThat(rowSecondInstance).isEqualTo(rowFirstInstance);
 
         // Should not mutate existing instance
-        Seat A3 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
-        rowSecondInstance.addSeat(A3);
-        assertThat(rowSecondInstance).isEqualTo(rowFirstInstance);
+        Seat a3 = new Seat("A", 2, PricingCategory.Second, SeatAvailability.Available);
+        Row newRowWithNewSeatAdded = rowSecondInstance.addSeat(a3);
+        assertThat(newRowWithNewSeatAdded).isNotEqualTo(rowFirstInstance);
     }
+
 
     @Test
     public void Offer_seats_from_the_middle_of_the_row_when_the_row_size_is_even_and_party_size_is_greater_than_one() {
