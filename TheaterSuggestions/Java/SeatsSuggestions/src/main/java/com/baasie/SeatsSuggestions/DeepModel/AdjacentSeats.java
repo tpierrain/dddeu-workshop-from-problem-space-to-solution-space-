4acc7498566cf4717068af14d5acd79a28c46aca
Bruno Boucard <boucard.bruno@free.fr>
2021-10-24 11:09:08 +0200

    Refactoring OfferingAdjacentSeatsToMembersOfTheSameParty - remove Primitive Obsession code smell

---
+++ TheaterSuggestions/Java/SeatsSuggestions/src/main/java/com/baasie/SeatsSuggestions/DeepModel/AdjacentSeats.java
@@ -0,0 +1,21 @@
+package com.baasie.SeatsSuggestions.DeepModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdjacentSeats {
+    public ArrayList<SeatWithDistance> seatsWithDistance = new ArrayList<>();
+
+    public AdjacentSeats() {
+    }
+
+    public AdjacentSeats(List<SeatWithDistance> seats)
+    {
+        seatsWithDistance.addAll(seats);
+    }
+
+    public void addSeat(SeatWithDistance seatWithTheDistance)
+    {
+        seatsWithDistance.add(seatWithTheDistance);
+    }
+}
---
+++ TheaterSuggestions/Java/SeatsSuggestions/src/main/java/com/baasie/SeatsSuggestions/DeepModel/AdjacentSeatsGroups.java
@@ -0,0 +1,13 @@
+package com.baasie.SeatsSuggestions.DeepModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AdjacentSeatsGroups {
+    public List<AdjacentSeats> groups;
+
+    public AdjacentSeatsGroups() {
+        groups = new ArrayList<>();
+    }
+}
