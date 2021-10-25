1b0dcaaeb8129914ee28a830cdcc466b510456e2
Bruno Boucard <boucard.bruno@free.fr>
2021-10-25 15:46:15 +0200

    Refactoring OfferingAdjacentSeatsToMembersOfTheSameParty - remove Primitive Obsession code smell

--- TheaterSuggestions/Java/SeatsSuggestionsDomain/src/main/java/com/baasie/SeatsSuggestionsDomain/PartyRequested.java
+++ TheaterSuggestions/Java/SeatsSuggestionsDomain/src/main/java/com/baasie/SeatsSuggestionsDomain/PartyRequested.java
@@ -5,13 +5,13 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode
 public class PartyRequested {
 
-    public static final int MAX_PARTY_SIZE = 20;
+    public static final int MAX_PARTY_SIZE = 6;
     private final int _partySize;
 
     public PartyRequested(int partySize)
     {
         if (partySize <= 0 || partySize > MAX_PARTY_SIZE)
-            throw new IllegalArgumentException("Parameter 'partySize' should be greater than zero and less than 20");
+            throw new IllegalArgumentException("Parameter 'partySize' should be greater than zero and less than 6");
 
         _partySize = partySize;
     }
