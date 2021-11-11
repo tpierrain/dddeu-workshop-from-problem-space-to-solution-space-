57883efb0bc44b22f959184a80f044b0e83fdf07
Bruno Boucard <boucard.bruno@free.fr>
2021-11-11 16:38:42 +0100

    starting point for BetClic

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocatorShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocatorShould.cs
@@ -12,25 +12,33 @@ namespace SeatsSuggestions.Tests
         [Test]
         public void Suggest_one_seat_when_Auditorium_contains_one_available_seat_only()
         {
-            // Example 1
+            // Example 1 - Happy pass
             //
-            // Ford Auditorium-1
+            // * Party 1
+            //
+            // * Ford Auditorium-1
             //
             //       1   2   3   4   5   6   7   8   9  10
             //  A : (2) (2)  1  (1) (1) (1) (1) (1) (2) (2)
             //  B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
+            //
+            // => A3
            
         }
 
         [Test]
-        public void Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved()
+        public void Return_SuggestionNotAvailable_when_Auditorium_has_all_its_seats_already_reserved()
         {
-            // Example 2
+            // Example 2 - Unhappy pass
             //
-            // Madison Auditorium-5
+            // * Party 1
+            //
+            // * Madison Auditorium-5
             //      1   2   3   4   5   6   7   8   9  10
             // A : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
             // B : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
+            //
+            // => SuggestionNotAvailable
 
         }
     }
