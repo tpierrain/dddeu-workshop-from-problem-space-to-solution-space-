1ee5222be47b7ff29a2c47939b1ced44fce99f65
Bruno Boucard <boucard.bruno@free.fr>
2021-11-11 15:37:55 +0100

    starting point for BetClic

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocatorShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocatorShould.cs
@@ -6,17 +6,14 @@ namespace SeatsSuggestions.Tests
     public class SeatAllocatorShould
     {
         /*
-         * By respecting the Outside/In approach of Test Driven Development with the emerging design,
-         * you should bring out the SeatAllocator service to produce your first seat suggestions
-         * for a given party and a given show ID.
-         *
-         * We offer you two concrete examples to allow you to realize
-         * a happy path case and a failure case.
+         *  Business Rule - Only Suggest available seats
          */
 
         [Test]
         public void Suggest_one_seat_when_Auditorium_contains_one_available_seat_only()
         {
+            // Example 1
+            //
             // Ford Auditorium-1
             //
             //       1   2   3   4   5   6   7   8   9  10
@@ -28,6 +25,8 @@ namespace SeatsSuggestions.Tests
         [Test]
         public void Return_SeatsNotAvailable_when_Auditorium_has_all_its_seats_already_reserved()
         {
+            // Example 2
+            //
             // Madison Auditorium-5
             //      1   2   3   4   5   6   7   8   9  10
             // A : (2) (2) (1) (1) (1) (1) (1) (1) (2) (2)
