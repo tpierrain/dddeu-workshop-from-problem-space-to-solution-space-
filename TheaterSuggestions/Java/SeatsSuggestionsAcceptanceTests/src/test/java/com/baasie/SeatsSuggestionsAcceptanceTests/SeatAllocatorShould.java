613d89125628ec4fa988eca3abf6b68546b8a6b7
Bruno Boucard <boucard.bruno@free.fr>
2021-11-18 09:37:35 +0100

    comment improvement

--- TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatAllocatorShould.java
+++ TheaterSuggestions/Java/SeatsSuggestionsAcceptanceTests/src/test/java/com/baasie/SeatsSuggestionsAcceptanceTests/SeatAllocatorShould.java
@@ -4,14 +4,8 @@ import org.junit.Test;
 
 public class SeatAllocatorShould {
     /*
-     * By respecting the Outside/In approach of Test Driven Development with the emerging design,
-     * you should bring out the SeatAllocator service to produce your first seat suggestions
-     * for a given party and a given show ID.
-     *
-     * We offer you two concrete examples to allow you to realize
-     * a happy path case and a failure case.
+     *  Business Rule - Only Suggest available seats
      */
-
     @Test
     public void suggest_one_seat_when_Auditorium_contains_one_available_seat_only() {
         // Example 1 - Happy path
