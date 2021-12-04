349bf721ba90d40f5c4e30cf8e57790759a0bf66
Bruno Boucard <boucard.bruno@free.fr>
2021-12-04 17:28:46 +0100

    rewording test names

--- TheaterSuggestions/Java/SeatsSuggestionsApi/src/main/java/com/baasie/SeatsSuggestionsApi/controllers/SeatSuggestionsController.java
+++ TheaterSuggestions/Java/SeatsSuggestionsApi/src/main/java/com/baasie/SeatsSuggestionsApi/controllers/SeatSuggestionsController.java
@@ -3,10 +3,8 @@ package com.baasie.SeatsSuggestionsApi.controllers;
 import com.baasie.ExternalDependencies.auditoriumlayoutrepository.AuditoriumLayoutRepository;
 import com.baasie.ExternalDependencies.reservationsprovider.ReservationsProvider;
 import com.baasie.SeatsSuggestions.AuditoriumSeatingAdapter;
-import com.baasie.SeatsSuggestions.SeatAllocator;
+import com.baasie.SeatsSuggestions.SeatsAllocator;
 import com.baasie.SeatsSuggestions.SuggestionsMade;
-import com.baasie.ExternalDependencies.IProvideAuditoriumLayouts;
-import com.baasie.ExternalDependencies.IProvideCurrentReservations;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -23,7 +21,7 @@ public class SeatSuggestionsController {
     @GetMapping(produces = "application/json")
     public SuggestionsMade get(@RequestParam String showId, @RequestParam int party) throws IOException {
 
-        SeatAllocator seatAllocator = new SeatAllocator(new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider()));
-        return seatAllocator.makeSuggestions(showId, party);
+        SeatsAllocator seatsAllocator = new SeatsAllocator(new AuditoriumSeatingAdapter(new AuditoriumLayoutRepository(), new ReservationsProvider()));
+        return seatsAllocator.makeSuggestions(showId, party);
     }
 }
